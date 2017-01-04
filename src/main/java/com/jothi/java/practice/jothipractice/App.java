ad3ccc69d3e3d0e0b46fdb1e567e85cdcea13236
cvaijothi <cvaijothi@gmail.com>
2017-01-04 22:11:03 +0530

    first push

---
+++ src/main/java/com/jothi/java/practice/jothipractice/App.java
@@ -0,0 +1,13 @@
+package com.jothi.java.practice.jothipractice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
